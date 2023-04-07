4d22ef59b04544747753ec2d3096b3c173c172f2
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-04-07 11:42:55 +0700

    update globals.py

--- Azazel/core/SQL/globals.py
+++ Azazel/core/SQL/globals.py
@@ -93,7 +93,7 @@ def gvarstatus(user_id, variable):
 
 
 def addgvar(user_id, variable, value):
-    if SESSION.query(Globals).filter(Globals.user_id == str(user_id)).all():
+    if SESSION.query(Globals).filter(Globals.variable == str(variable)).all():
         delgvar(user_id, variable)
     adder = Globals(user_id, variable, value)
     SESSION.add(adder)
